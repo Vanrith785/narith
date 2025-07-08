c6ab637eeaebb00b4dd77c70b3e69acca07fcf05
Vanrith <pichvanrith785@gmail.com>
2025-07-08 14:05:04 +0700

    g

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -48,14 +48,14 @@ export default {
         {
           title: 'Spring and Winter i',
           author: 'William Shakespeare',
-          img: '/IMG_6159.JPG',
+          img: '94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif',
           link: '/blog/spring-winter-i',
           updated: '3 mins ago'
         },
         {
           title: 'Spring',
           author: 'William Shakespeare',
-          img: '/IMG_5720.JPG',
+          img: '/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif',
           link: '/blog/spring',
           updated: '3 mins ago'
         }
